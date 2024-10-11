1f78d912083a971d80f07a0b70dfa511c52a6c9a
intibal29 <intissar.balouk@federicobaraibar.com>
2024-10-11 13:35:34 +0200

    Actividad 11

---
+++ UD1/ACT11/Actividad11.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Actividad11 {
+    public static void main(String[] args){
+        Runtime r = Runtime.getRuntime();
+        String comando = "ls -al"; // Comando para listar archivos en Linux
+        Process p = null;
+        try {
+            p = r.exec(comando);
+            InputStream is = p.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String linea;
+            while((linea = br.readLine()) != null) // lee una linea
+                System.out.println(linea);
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Comprobación: Valor= 0 bien, Valor = -1 mal
+        int exitVal;
+        try {
+            exitVal = p.waitFor();
+            System.out.println("Valor de Salida " + exitVal);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

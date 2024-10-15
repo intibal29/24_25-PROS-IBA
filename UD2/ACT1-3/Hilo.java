0b89adf5c8d16f3f9d813822a8a58520beedbb7f
intibal29 <intissar.balouk@federicobaraibar.com>
2024-10-15 11:51:33 +0200

    Clase Hilo

---
+++ UD2/ACT1-3/Hilo.java
@@ -0,0 +1,19 @@
+
+// Clase Hilo que implementa la lógica tanto de Primero como de Segundo
+class Hilo extends Thread {
+    private String nombre;
+
+    // Constructor que recibe el nombre del hilo
+    public Hilo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Método run que ejecuta la lógica del hilo
+    @Override
+    public void run() {
+        // Se imprime el nombre del hilo 20 veces
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(nombre + " " + i);
+        }
+    }
+}

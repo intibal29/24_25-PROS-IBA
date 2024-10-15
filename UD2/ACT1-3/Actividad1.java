59ec57faf400c41d943c1d51c126846952070bb9
intibal29 <intissar.balouk@federicobaraibar.com>
2024-10-15 11:50:48 +0200

    Clase Actividad1

---
+++ UD2/ACT1-3/Actividad1.java
@@ -0,0 +1,16 @@
+public class Actividad1 {
+    public static void main(String[] args) {
+        // Crear dos instancias de la clase Hilo con nombres diferentes
+        Hilo primero = new Hilo("Primero");
+        Hilo segundo = new Hilo("Segundo");
+
+        // Iniciar ambos hilos
+        primero.start();
+        segundo.start();
+
+        // Mensaje final después de que ambos hilos se hayan iniciado
+        System.out.println("Fin programa");
+    }
+}
+
+

354064c2717c5b0be45465a726f15d0bdc344338
intibal29 <intissar.balouk@federicobaraibar.com>
2024-10-20 20:48:56 +0200

    UD2.ACT2.intento1.definitivo

---
+++ UD2/ACT1-3/Actividad2.java
@@ -0,0 +1,31 @@
+public class Actividad2 {
+    public static void main(String[] args) {
+        // Recibe n como argumento (convertido a entero)
+        int n = Integer.parseInt(args[0]);
+
+        // Crear e iniciar n hilos
+        for (int i = 1; i <= n; i++) {
+            Hilo hilo = new Hilo(i);
+            hilo.start();
+        }
+
+        // Mensaje final después de crear los hilos
+        System.out.println("Final Programa");
+    }
+}
+
+class Hilo extends Thread {
+    private int numeroHilo;
+
+    public Hilo(int numeroHilo) {
+        this.numeroHilo = numeroHilo;
+    }
+
+    @Override
+    public void run() {
+        // Imprimir "Hilo n" 20 veces
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Hilo " + numeroHilo + " - iteración " + i);
+        }
+    }
+}
